4ecc3e35b8eec7e17c1f52f227c63858dab18a92
Sean Maller <sean.maller@live.co.uk>
2026-04-25 23:46:08 +0100

    implemented a common logout button for all users

---
+++ OrderForge.Client/Components/SignOutButtonVariant.cs
@@ -0,0 +1,13 @@
+namespace OrderForge.Client.Components;
+
+public enum SignOutButtonVariant
+{
+    PrimaryHeaderToolbar,
+
+    PrimaryHeaderToolbarLeadingMargin,
+
+    PrimaryHeaderMobileFullWidth,
+
+    /// <summary>For headers that are not the primary green bar (e.g. minimal shell).</summary>
+    NeutralToolbar,
+}
